70660a2069f0ca871eba1b886cdec2e36e494f13
rlopeslameira <rlopeslameira@gmail.com>
2024-12-13 10:44:03 -0300

    port

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from "express"
 import { routes } from "./routes"
 import { errorHandling } from "./middlewares/error-handling"
 
-const PORT = 3333
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
